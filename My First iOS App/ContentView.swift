e450adaa07417cc0075f3a9e8b806b42fc3ef0b3
heflinbh <heflinbh@miamioh.edu>
2022-07-08 13:27:05 +0100

    Initial Commit

---
+++ My First iOS App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My First iOS App
+//
+//  Created by Benjamin Heflin on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My First iOS App/My_First_iOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_iOS_AppApp.swift
+//  My First iOS App
+//
+//  Created by Benjamin Heflin on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct My_First_iOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
